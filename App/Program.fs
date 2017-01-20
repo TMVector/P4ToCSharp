25b52ed126b2e5a83c9ecd2eb49c3b6a6712df0f
Unknown <tmvector@gmail.com>
2017-01-20 22:55:10 +0000

    Code to generate a tree of the P4 IR types

--- App/Program.fs
+++ App/Program.fs
@@ -23,6 +23,41 @@ module Main =
 
 #if INTERACTIVE
 // FIXME this is testing code only
-let p4file = """C:\temp\header.p4.json"""
+let p4file = """C:\temp\core.p4.json"""
 Main.convertFile p4file
 #endif
+
+// Get inheritance tree of JsonTypes
+open System.Reflection
+module Option =
+  let ifNone defaultValue o =
+    match o with None -> defaultValue | Some v -> v
+module Temp =
+  type 'a tree = Br of 'a * 'a tree seq
+  let childrenOf (Br(_,ch)) = ch
+  let inNS (ns:string) =
+    if System.String.IsNullOrEmpty(ns) || not <| ns.Contains(".") then false
+    else
+      let ns = ns.Substring(ns.IndexOf('.') + 1)
+      ns = "P4ToCSharp.App.IR"
+  let ts =
+    Assembly.GetExecutingAssembly().GetTypes()
+    |> Seq.filter (fun t -> t.Namespace <> null && inNS t.Namespace)
+    |> Seq.map (fun t -> (if t.BaseType = null then "" else t.BaseType.Name), t.Name)
+    |> Seq.groupBy fst
+    |> Seq.map (fun (k,vs) -> k, vs |> Seq.map snd |> Seq.distinct)
+    |> Map.ofSeq
+  let tree =
+    let children = Map.toSeq ts |> Seq.map snd |> Seq.concat |> Seq.distinct |> Set.ofSeq
+    let topLevel = Map.toSeq ts |> Seq.map fst |> Set.ofSeq |> Set.difference <| children
+    let rec treeOf ty =
+      let children = ts.TryFind ty |> Option.ifNone Seq.empty
+      Br(ty, Seq.map treeOf children |> Seq.toArray)
+    topLevel |> Seq.map treeOf |> Seq.toArray
+  let nodeTree = (childrenOf tree.[2] |> Seq.toArray).[1]
+  let writeTree tr =
+    let indent str = "  " + str
+    let rec draw (Br(n,ch)) =
+      seq { yield n; yield! ch |> Seq.sortBy (fun (Br(n,_)) -> n) |> Seq.map (draw >> (Seq.map indent)) |> Seq.concat}
+    System.IO.File.WriteAllLines("""C:\temp\tree.txt""", draw tr)
+  writeTree nodeTree
