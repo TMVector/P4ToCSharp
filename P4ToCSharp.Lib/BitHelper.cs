faf04412f3db4689c6499b175b22d3d5305b319a
TMVector <tmvector@gmail.com>
2017-03-08 15:33:00 +0000

    Update BitHelper methods to use bitOffsets
    Also added assertions for byte boundaries and changed Extract48 to only read 6 bytes, instead of reading 8 and then masking.

--- P4ToCSharp.Lib/BitHelper.cs
+++ P4ToCSharp.Lib/BitHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace P4ToCSharp.Library
   public static class BitHelper
   {
     // FIXME perhaps better to have internal methods that extract C# datatypes, and public ones that wrap it as bitN?
-    public static bit1 ExtractBit(byte[] arr, uint bitOffset)
+    public static bit1 Extract1(byte[] arr, uint bitOffset)
     {
       uint startByte = bitOffset / 8;
       uint localBitOffset = bitOffset % 8;
@@ -28,51 +29,67 @@ namespace P4ToCSharp.Library
       return (ushort)data;
     }
 
-    public static bit8 Extract8(byte[] arr, uint offset)
+    public static bit4 Extract4(byte[] arr, uint bitOffset)
     {
-      return new bit8(arr[offset]);
+      
     }
 
-    public static unsafe bit16 Extract16(byte[] arr, uint offset)
+    public static bit8 Extract8(byte[] arr, uint bitOffset)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Extract8 must be a multiple of 8");
+      return new bit8(arr[bitOffset / 8]);
+    }
+
+    public static unsafe bit16 Extract16(byte[] arr, uint bitOffset)
+    {
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Extract16 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         return new bit16(*((ushort*)p));
       }
     }
 
-    public static unsafe bit32 Extract32(byte[] arr, uint offset)
+    public static unsafe bit32 Extract32(byte[] arr, uint bitOffset)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Extract32 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         return new bit32(*((uint*)p));
       }
     }
 
-    public static bit48 Extract48(byte[] arr, uint offset)
+    public static unsafe bit48 Extract48(byte[] arr, uint bitOffset)
     {
-      // FIXME is it okay to extract 64 bits when we don't know it's safe? :S
-      return new bit48(Extract64(arr, offset).Value & 0x0000FFFFFFFFFFFFuL);
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Extract48 must be a multiple of 8");
+      ulong rv;
+      fixed (byte* p = &arr[bitOffset / 8])
+      {
+        rv = *((uint*)p);
+        rv |= ((ulong)*((ushort*)(p + 4))) << 32;
+      }
+      return new bit48(rv);
     }
 
-    public static unsafe bit64 Extract64(byte[] arr, uint offset)
+    public static unsafe bit64 Extract64(byte[] arr, uint bitOffset)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Extract64 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         return new bit64(*((ulong*)p));
       }
     }
 
-    public static byte[] ExtractBytes(byte[] arr, uint offset, uint length)
+    public static byte[] ExtractBytes(byte[] arr, uint bitOffset, uint length)
     {
+      Debug.Assert(bitOffset % 8 == 0, "Offset for ExtractBytes must be a multiple of 8");
       byte[] r = new byte[length];
-      Buffer.BlockCopy(arr, (int)offset, r, 0, (int)length);
+      Buffer.BlockCopy(arr, (int)(bitOffset / 8), r, 0, (int)length);
       return r;
     }
 
     // FIXME: bounds on Write methods
 
-    public static void WriteBit(byte[] arr, uint bitOffset, bool value)
+    public static void Write1(byte[] arr, uint bitOffset, bool value)
     {
       uint startByte = bitOffset / 8;
       uint localBitOffset = bitOffset % 8;
@@ -94,47 +111,52 @@ namespace P4ToCSharp.Library
       Write32(arr, startByte, new bit32(data));
     }
 
-    public static void Write8(byte[] arr, uint offset, bit8 value)
+    public static void Write8(byte[] arr, uint bitOffset, bit8 value)
     {
-      arr[offset] = value.Value;
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Write8 must be a multiple of 8");
+      arr[bitOffset / 8] = value.Value;
     }
 
-    public static unsafe void Write16(byte[] arr, uint offset, bit16 value)
+    public static unsafe void Write16(byte[] arr, uint bitOffset, bit16 value)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Write16 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         *((ushort*)p) = value.Value;
       }
     }
 
-    public static unsafe void Write32(byte[] arr, uint offset, bit32 value)
+    public static unsafe void Write32(byte[] arr, uint bitOffset, bit32 value)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Write32 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         *((uint*)p) = value.Value;
       }
     }
 
-    public static unsafe void Write48(byte[] arr, uint offset, bit48 value)
+    public static unsafe void Write48(byte[] arr, uint bitOffset, bit48 value)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Write48 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         *((uint*)p) = (uint)value.Value;
         *((ushort*)(p + 4)) = (ushort)(value.Value >> 32);
       }
     }
 
-    public static unsafe void Write64(byte[] arr, uint offset, bit64 value)
+    public static unsafe void Write64(byte[] arr, uint bitOffset, bit64 value)
     {
-      fixed (byte* p = &arr[offset])
+      Debug.Assert(bitOffset % 8 == 0, "Offset for Write64 must be a multiple of 8");
+      fixed (byte* p = &arr[bitOffset / 8])
       {
         *((ulong*)p) = value.Value;
       }
     }
 
-    public static void WriteBytes(byte[] arr, uint offset, uint length, byte[] value)
+    public static void WriteBytes(byte[] arr, uint byteOffset, uint length, byte[] value)
     {
-      Buffer.BlockCopy(value, 0, arr, (int)offset, (int)length);
+      Buffer.BlockCopy(value, 0, arr, (int)byteOffset, (int)length);
     }
 
     // FIXME I can see this is going to be a pain with the bitN structs...
