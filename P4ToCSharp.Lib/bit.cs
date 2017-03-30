be6d3e903cb8685ae20944b1348b14e79b4b32cb
TMVector <tmvector@gmail.com>
2017-03-30 23:41:49 +0100

    Update bitN implementations
    Make consistent, and add arithemetic to structs that correspond to C# primitives.

--- P4ToCSharp.Lib/bit.cs
+++ P4ToCSharp.Lib/bit.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace P4ToCSharp.Library
 {
   // FIXME make all these consistent and handle weird sizes either dynamically or by generating another struct type
-  // FIXME is arithmetic defined for bit<N>? vss-example does ttl-1
+  // FIXME is arithmetic defined for bit<N>? vss-example does ttl-1. What about bitN op bitM?
+  //       In my proposal I said I would only handle arithmetic on types that are the same width as C# primitives.
 
   public interface IBitString
   {
@@ -23,21 +24,54 @@ namespace P4ToCSharp.Library
 #if KIWI
     [Kiwi.HwWidth(BitWidth)]
 #endif
-    public Byte Value { get; }
+    public byte Value { get; }
 
 
-    public bit1(Byte val)
+    public bit1(byte val)
     {
-      Value = val;
+      Debug.Assert(val < (1u << BitWidth));
+      Value = val; // FIXME mask? (Debug.Assert is not checked in Release)
     }
 
     public static bool operator ==(bit1 a, bit1 b)
     {
       return a.Value == b.Value;
     }
+
+    public static bool operator ==(bit1 a, byte b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit1 a, bit1 b)
     {
       return !(a == b);
+    }
+    public static bool operator !=(bit1 a, byte b)
+    {
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit1)
+        return this == (bit1)obj;
+      else if (obj is byte)
+        return this == (byte)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return Value;
+    }
+
+    public static explicit operator bit1(uint v)
+    {
+      Debug.Assert(v < (1u << BitWidth));
+      return new bit1((byte)(v & ~(~0u << BitWidth)));
+    }
+    public static explicit operator uint(bit1 v)
+    {
+      return v.Value;
     }
   }
 
@@ -49,32 +83,50 @@ namespace P4ToCSharp.Library
 #if KIWI
     [Kiwi.HwWidth(BitWidth)]
 #endif
-    public Byte Value { get; }
+    public byte Value { get; }
 
-    public bit4(Byte val)
+    public bit4(byte val)
     {
-      Value = val;
+      Debug.Assert(val < (1u << BitWidth));
+      Value = val; // FIXME mask?
     }
 
     public static bool operator ==(bit4 a, bit4 b)
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit4 a, byte b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit4 a, bit4 b)
     {
       return !(a == b);
     }
+    public static bool operator !=(bit4 a, byte b)
+    {
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit4)
+        return this == (bit4)obj;
+      else if (obj is byte)
+        return this == (byte)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return Value;
+    }
 
-    public static explicit operator bit4(uint v) // FIXME is this the best way to handle InfInt conversion?
+    public static explicit operator bit4(uint v)
     {
       Debug.Assert(v < (1u << BitWidth));
-
-      return new bit4((byte)(v & ~(~1u << BitWidth)));
+      return new bit4((byte)(v & ~(~0u << BitWidth)));
     }
-    public static explicit operator bit4(int v) { return (bit4)(uint)v; }
-    public static explicit operator bit4(ushort v) { return (bit4)(uint)v; }
-
-    public static explicit operator int(bit4 v)
+    public static explicit operator uint(bit4 v)
     {
       return v.Value;
     }
@@ -96,13 +148,49 @@ namespace P4ToCSharp.Library
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit8 a, byte b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit8 a, bit8 b)
     {
       return !(a == b);
     }
-    public static bit8 operator -(bit8 a, int i)
+    public static bool operator !=(bit8 a, byte b)
     {
-      return new bit8((byte)(a.Value - i));
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit8)
+        return this == (bit8)obj;
+      else if (obj is byte)
+        return this == (byte)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return Value;
+    }
+
+    public static bit8 operator +(bit8 a, uint i)
+    {
+      return new bit8((byte)(a.Value - i)); // FIXME what about overflow, etc.? What is the P4 behaviour?
+    }
+    public static bit8 operator -(bit8 a, uint i)
+    {
+      return new bit8((byte)(a.Value - i)); // FIXME overflow
+    }
+
+    public static explicit operator bit8(uint v)
+    {
+      Debug.Assert(v < (1u << BitWidth));
+      return new bit8((byte)(v & ~(~0u << BitWidth)));
+    }
+    public static explicit operator uint(bit8 v)
+    {
+      return v.Value;
     }
   }
 
@@ -122,17 +210,47 @@ namespace P4ToCSharp.Library
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit16 a, UInt16 b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit16 a, bit16 b)
     {
       return !(a == b);
     }
-
-    public static explicit operator bit16(int v)
+    public static bool operator !=(bit16 a, UInt16 b)
     {
-      // FIXME implement casts
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit16)
+        return this == (bit16)obj;
+      else if (obj is UInt16)
+        return this == (UInt16)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return Value;
     }
 
-    public static explicit operator ushort(bit16 v)
+    public static bit16 operator +(bit16 a, uint i)
+    {
+      return new bit16((UInt16)(a.Value - i)); // FIXME what about overflow, etc.? What is the P4 behaviour?
+    }
+    public static bit16 operator -(bit16 a, uint i)
+    {
+      return new bit16((UInt16)(a.Value - i)); // FIXME overflow
+    }
+
+    public static explicit operator bit16(uint v)
+    {
+      Debug.Assert(v < (1u << BitWidth));
+      return new bit16((UInt16)(v & ~(~0u << BitWidth)));
+    }
+    public static explicit operator uint(bit16 v)
     {
       return v.Value;
     }
@@ -154,9 +272,49 @@ namespace P4ToCSharp.Library
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit32 a, UInt32 b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit32 a, bit32 b)
     {
       return !(a == b);
+    }
+    public static bool operator !=(bit32 a, UInt32 b)
+    {
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit32)
+        return this == (bit32)obj;
+      else if (obj is UInt32)
+        return this == (UInt32)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return (int)Value;
+    }
+
+    public static bit32 operator +(bit32 a, uint i)
+    {
+      return new bit32(a.Value - i);
+    }
+    public static bit32 operator -(bit32 a, uint i)
+    {
+      return new bit32(a.Value - i);
+    }
+
+    public static explicit operator bit32(uint v)
+    {
+      Debug.Assert(v < (1u << BitWidth));
+      return new bit32(v & ~(~0u << BitWidth));
+    }
+    public static explicit operator uint(bit32 v)
+    {
+      return v.Value;
     }
   }
 
@@ -172,6 +330,7 @@ namespace P4ToCSharp.Library
 
     public bit48(UInt64 val)
     {
+      Debug.Assert(val < (1uL << BitWidth));
       Value = val;
     }
 
@@ -179,9 +338,40 @@ namespace P4ToCSharp.Library
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit48 a, UInt64 b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit48 a, bit48 b)
     {
       return !(a == b);
+    }
+    public static bool operator !=(bit48 a, UInt64 b)
+    {
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit48)
+        return this == (bit48)obj;
+      else if (obj is UInt64)
+        return this == (UInt64)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return (int)Value;
+    }
+
+    public static explicit operator bit48(ulong v)
+    {
+      Debug.Assert(v < (1uL << BitWidth));
+      return new bit48(v & ~(~0uL << BitWidth));
+    }
+    public static explicit operator ulong(bit48 v)
+    {
+      return v.Value;
     }
   }
 
@@ -201,9 +391,49 @@ namespace P4ToCSharp.Library
     {
       return a.Value == b.Value;
     }
+    public static bool operator ==(bit64 a, UInt64 b)
+    {
+      return a.Value == b;
+    }
     public static bool operator !=(bit64 a, bit64 b)
     {
       return !(a == b);
+    }
+    public static bool operator !=(bit64 a, UInt64 b)
+    {
+      return !(a == b);
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj is bit64)
+        return this == (bit64)obj;
+      else if (obj is UInt64)
+        return this == (UInt64)obj;
+      else
+        return false;
+    }
+    public override int GetHashCode()
+    {
+      return (int)Value;
+    }
+
+    public static bit64 operator +(bit64 a, ulong i)
+    {
+      return new bit64(a.Value - i);
+    }
+    public static bit64 operator -(bit64 a, ulong i)
+    {
+      return new bit64(a.Value - i);
+    }
+
+    public static explicit operator bit64(ulong v)
+    {
+      Debug.Assert(v < (1uL << BitWidth));
+      return new bit64(v & ~(~0uL << BitWidth));
+    }
+    public static explicit operator ulong(bit64 v)
+    {
+      return v.Value;
     }
   }
 }
