fb8262a9236edfe536d4e88202793ff800599a4c
TMVector <tmvector@gmail.com>
2017-04-24 22:58:34 +0100

    Fix paket and add test project for P4ToCSharp.Lib
    - tell paket to target framework .NET 4.5
    - specify version of Rolsyn packages to 1.*, since 2+ don't include targets for net45
    - set framework version to .NET 4.5 for all projects since paket doesn't like 4.5.2 smh

---
+++ P4ToCSharp.Lib.Test/Bit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace P4ToCSharp.Lib.Test
+{
+  public class BitTests
+  {
+    [Fact]
+    public void TestMethod1()
+    {
+      Assert.True(false);
+    }
+  }
+}
