bf9397c4ca1c12dfcd6320103fdf453675488ddc
TMVector <tmvector@gmail.com>
2017-05-14 02:05:10 +0100

    Add tables for range/selector in v1model
    - Add placeholder table classes for range and selector
    - Add the P4 source files to v1model/vss-model projects for easy access

---
+++ v1model/RangeLookup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using P4ToCSharp.Library;
+
+namespace v1model
+{
+  [P4Lookup("range")]
+  public sealed class RangeTable<TKey, TResult> : ILookup<TKey, TResult>
+  {
+    private TKey[] Keys;
+    private TResult[] Values;
+
+    public TResult this[TKey key]
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+  }
+}
