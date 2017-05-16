dc15f6716898d0e24928fe8139f8028705857635
TMVector <tmvector@gmail.com>
2017-05-16 01:35:42 +0100

    Add logs of transpile/compile for convenience

--- v1model/transpile_compile.bat
+++ v1model/transpile_compile.bat
@@ -3,7 +3,7 @@ REM Install csc for C#6
 REM C:\temp\nuget.exe install Microsoft.Net.Compilers
 
 REM Set up variables
-set csc="C:\working\part-ii-project\P4ToCSharp\HandConverted\P4lang\P4c\P4_16_samples\bmv2\Microsoft.Net.Compilers.2.1.0\tools\csc.exe"
+set csc="C:\temp\p4\Microsoft.Net.Compilers.2.1.0\tools\csc.exe"
 set P4TOCS="C:\working\part-ii-project\P4ToCSharp\App\bin\Debug\p4tocs.exe"
 set p4tocslib="P4ToCSharp.Lib.dll"
 set v1model="v1model.dll"
@@ -22,3 +22,7 @@ FOR %%F in (*.stf) DO (
 	"%%~nF.exe" %%~nF.stf > %%~nF.log 2>&1
   )
 )
+
+REM for /F "tokens=*" %F in (C:\working\part-ii-project\P4ToCSharp\v1model\v1model-p4c_16_corpus.txt) do cp %~nF* v1model\
+REM for /R %F in (*.p4) do %p4tocs% generate-program %~nF.p4.json --architecture-library %v1model% >> transpile.log 2>&1
+REM for /R %F in (*.p4) do IF EXIST "%~nF.p4.json.gen.cs" ( %csc% /t:exe /out:%~nF.exe "%~nF.p4.json.gen.cs" /r:%v1model% /r:%p4tocslib% >> compile.log)
